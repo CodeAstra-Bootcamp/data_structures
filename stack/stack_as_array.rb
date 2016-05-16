16daba593fe446a62f3f302ede48acd3d3222dbd
B V Satyaram <bvsatyaram@gmail.com>
2016-05-16 15:05:15 +0530

    Implement Stack using Array

---
+++ stack/stack_as_array.rb
@@ -0,0 +1,27 @@
+class Stack < Array
+  attr_accessor :tail_index
+
+  def push(val)
+    raise "StackOverflow" if self.tail_index == self.length - 1
+
+    self.tail_index || = -1
+    self.tail_index += 1
+    self[self.tail_index] = val
+  end
+
+  def pop
+    raise "StackUnderflow" if self.tail_index.nil?
+
+    val = self[self.tail_index]
+    self.tail_index -= 1
+    self.tail_index = nil if self.tail_index == -1
+
+    return val
+  end
+
+  def peep
+    return nil if self.tail_index.nil?
+
+    return self[self.tail_index]
+  end
+end
